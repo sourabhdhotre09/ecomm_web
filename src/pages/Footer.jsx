95c03ae904830fde056ba02be329eb552ee2da57
s.dhotre <sdhotresnt>
2025-05-12 19:54:16 +0530

    footer content change

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
    <>
     <footer className='bg-brand py-3 text-center'>
       <div className="logo_outer text-center"><img src={logo} alt="" /><span className="logo_title">SD HUB</span></div>
-      <div className="text-center">@ 2025 Copyright By Saurabh Dhotre </div>
+      <div className="text-center">@ 2025 Developed By Saurabh Dhotre </div>
     </footer>
    </>
   )
